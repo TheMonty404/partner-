a2ae8f38db53d0992199e7892867538ba7c723e1
slawwwy <76216036+slawwwy@users.noreply.github.com>
2021-01-17 22:35:47 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({disableEveryone: true})
+
+
+client.on('message', message => { 
+
+if (!message.guild) return; 
+
+if (message.content === 'p+wara') { 
+ if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+if (message.member.voiceChannel) { 
+
+message.member.voiceChannel.join() 
+
+.then(connection => { 
+
+
+}) 
+
+.catch(console.log); 
+
+} else { 
+
+message.reply('**ئەگەر بەخۆت نێیە ڤۆیس ئەمنیش نایێم😂**'); 
+
+} 
+
+} 
+
+});
+
+
+client.on("message" , message => {
+
+if(message.channel.type === 'dm') {
+
+if (message.content.startsWith("https://discord.gg/")) {
+    
+  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/EXCuvNd`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+    
+  client.channels.get("755636700482633791").send(
+`> <:ARBELE_UPUP:791411050238836766> By: <@${message.author.id}> ✅ 
+${message.content}`)
+}
+    }
+        })
+
+
+
+
+client.on('ready', () => {
+console.log(`ON ${client.guilds.size} Servers`);
+console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`ڕیکلامەکە بنێرە بۆمن دایدەنێم☑️`) 
+});
+
+
+
+
+client.login("NzQ3NTI0NzI3NDg4MjQ5OTM2.X0QIpw.GTea_z0CwoeJjub_4q7I34WVgoc");//تۆکین لێرە دانێ
