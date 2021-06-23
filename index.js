8b2d1c6e2f93fa495ff4e6cc8ecefeb37da1f5cb
Mooonty <82681184+Mooonty@users.noreply.github.com>
2021-06-23 20:48:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ if (message.content.startsWith("https://discord.gg/")) {
     
   message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/AkYA68Z2gG`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
-  client.channels.get("851563689756196877").send(
+  client.channels.get("841792552314994709").send(
 `> <a:E14567BBEE164342AF58DF6872011807:853287588390633502> By: <@${message.author.id}> ✅ 
 ${message.content}`)
 }
