c97728f2c86cc70e4cf1bac077f81b29c9c63adb
TheMonty <82681184+TheMonty404@users.noreply.github.com>
2021-09-22 01:09:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ message.member.voiceChannel.join()
 
 } else { 
 
-message.reply('**<a:9FAF7C0CF0104A2F979F1263DF55D91A:855230044258828328> پێویستە تۆ لە ڤۆیسەکە بیت **'); 
+message.reply('** پێویستە تۆ لە ڤۆیسەکە بیت **'); 
 
 } 
 
@@ -38,8 +38,8 @@ if (message.content.startsWith("https://discord.gg/")) {
     
   message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/AkYA68Z2gG`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
-  client.channels.get("841792552314994709").send(
-`> <a:E14567BBEE164342AF58DF6872011807:853287588390633502> By: <@${message.author.id}> ✅ 
+  client.channels.get("id Channel ").send(
+`> By: <@${message.author.id}> ✅ 
 ${message.content}`)
 }
     }
@@ -51,10 +51,10 @@ ${message.content}`)
 client.on('ready', () => {
 console.log(`ON ${client.guilds.size} Servers`);
 console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Rose 💙`) 
+client.user.setGame(`Monty404 is here`) 
 });
 
 
 
 
-client.login("");// Your Token
+client.login("");// Token Lera Dane
